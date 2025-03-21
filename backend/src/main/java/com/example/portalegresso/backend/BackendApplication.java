4dbfa23e2a82b71c9f065a4db062b7fc33835a73
ucasabreu <ucasabreu1707@gmail.com>
2025-03-20 21:49:28 -0300

    update CORS configuration to allow requests from localhost:9090

--- backend/src/main/java/com/example/portalegresso/backend/BackendApplication.java
+++ backend/src/main/java/com/example/portalegresso/backend/BackendApplication.java
@@ -21,7 +21,7 @@ public class BackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:5173")
+					.allowedOrigins("http://localhost:9090") 
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 			}
 		};
